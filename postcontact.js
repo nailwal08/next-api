49eead5a14ca999aef08f5889aaa7f39cd9f3be9
NIKHIL NAILWAL <78683932+nailwal08@users.noreply.github.com>
2024-03-10 20:03:21 +0530

    Added delete API

--- postcontact.js
+++ postcontact.js
@@ -1,6 +1,7 @@
 //path of this file => create-next-app/pages/api/postcontact.js
 
 import * as fs from 'fs';
+import path from 'path';
 
 export default async function handler(req, res){
     if(req.method === 'POST'){
@@ -12,7 +13,22 @@ export default async function handler(req, res){
             console.error('Error writing file:', err);
             res.status(500).json({ error: 'Error writing file' });
         }
-    } else {
-        res.status(200).json(["data"]);
     }
+    else if(req.method === 'DELETE'){
+        try {
+            const { filename } = req.body; // Extract filename from req.body
+            if (!filename) {
+                return res.status(400).json({ error: 'Filename not provided' });
+            }
+            const filePath = path.join('contactdata', `${filename}.json`);
+            await fs.promises.unlink(filePath);
+            res.status(200).json({ message: 'File deleted successfully' });
+        } catch (err) {
+            console.error('Error deleting file:', err);
+            res.status(500).json({ error: 'Error deleting file' });
+        }
+    } 
+    else {
+        res.status(405).json({ error: 'Method Not Allowed' });
+    } 
 }
