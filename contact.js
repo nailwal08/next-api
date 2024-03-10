3153a49a3b40e1281ae29f2f5698fb104f2cf955
NIKHIL NAILWAL <78683932+nailwal08@users.noreply.github.com>
2024-03-10 20:02:39 +0530

    Added delete API

--- contact.js
+++ contact.js
@@ -3,11 +3,32 @@
 import React, { useState } from 'react'
 import styles from '../src/app/contact.module.css'
 
+
 const Contact = () => {
   const [name, setname] = useState('')
   const [email, setemail] = useState('')
   const [phone, setphone] = useState('')
   const [desc, setdesc] = useState('')
+  const [delfile, setdelfile] = useState('')
+
+  const handleDelete = (e) => {
+    e.preventDefault();
+    const data = { delfile }
+    fetch('http://localhost:3000/api/postcontact',{
+      method: 'DELETE',
+      handlers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(response => response.text())
+      .then(data => {
+        alert("File deleted Successfully.")
+        setdelfile('');
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -47,11 +68,14 @@ const Contact = () => {
     else if (e.target.name == 'desc') {
       setdesc(e.target.value)
     }
+    else if (e.target.name == 'delfile') {
+      setdelfile(e.target.value)
+    }
   }
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}>Contact us</h1>
-      <form onSubmit={handleSubmit}>
+      <form onDelete={handleDelete} onSubmit={handleSubmit}>
         <div className={styles.mb3}>
           <label htmlFor="name" className={styles.formlabel}>Enter your name</label>
           <input type="text" value={name} onChange={handleChange} className="form-control" id="name" name='name' aria-describedby="emailHelp" />
@@ -59,6 +83,7 @@ const Contact = () => {
         <div className={styles.mb3}>
           <label htmlFor="email" className={styles.formlabel}>Email address</label>
           <input type="email" value={email} onChange={handleChange} className="form-control" id="email" name='email' aria-describedby="emailHelp" />
+
         </div>
         <div className={styles.mb3}>
           <label htmlFor="phone" className={styles.formlabel}>Phone Number</label>
@@ -68,7 +93,12 @@ const Contact = () => {
         <div className={styles.mb3}>
           <textarea value={desc} onChange={handleChange} id="desc" placeholder="leave your message" className={styles.formlabel} rows={8} cols={38} name='desc' />
         </div>
+        <div className={styles.mb3}>
+          <label htmlFor="delfile" className={styles.formlabel}>Enter File Name</label>
+          <input type="text" value={delfile} onChange={handleChange} className="form-control" id="delfile" name='delfile' />
+        </div>
         <button type="submit" className={styles.btn}>Submit</button>
+        <button onClick={handleDelete} className={styles.btn}>Delete</button>
       </form>
     </div>
   )
